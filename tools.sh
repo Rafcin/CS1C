998ed09e66a64c3482dbe93d8dfeb7c59504ca3c
Raf <Rafcin.s@gmail.com>
2021-06-16 16:34:16 -0700

    Updated installer

--- tools.sh
+++ tools.sh
@@ -26,12 +26,24 @@ echo -e "
 +---------------------------+
 "
 
-PS3='Installer Options: '
-options=("Install" "Update", "Visual Studio Code Extensions" "Exit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Install")
+REQUIRED_PKG="whiptail"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG 
+fi
+
+if whiptail --yesno "Raf Workspace Installer | This script will install core pkgs for a better workflow, update pkgs and install vscode extensions. This pkg includes C++ tools, valgrind, vim, vscode, vscode with c++ formatting extensions, vbox fixes and other quality of life extensions." 10 100 --defaultno; then
+    CHOICE=$(whiptail --menu "Options" 18 100 10 \
+    "Install Tools" "Installs Visual Studio Code, Guake and all other tools." \
+    "Update" "Update all packages." \
+    "Visual Studio Code Extensions" "Adds useful extensions." 3>&1 1>&2 2>&3)
+    if [ -z "$CHOICE" ]; then
+    echo "No option was chosen (user hit Cancel)"
+    else
+        echo "The user chose $CHOICE"
+        if [[ "$CHOICE" == *"Install Tools"* ]]; then
             read -r -p "Are you sure you want to continue? [Y/N] " response
 			if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 			then
@@ -47,33 +59,25 @@ do
 				sudo apt install code
 
                 #kraken
-                sudo snap install gitkraken
+                sudo snap install gitkraken --classic
 
                 #discord
                 sudo snap install discord
 
                 #vbox extensions
+                #This fixes window size issues on latops and solves performance issues on OSX Lion and higher.
                 sudo apt-get install virtualbox-guest-additions-iso virtualbox-guest-x11
 
 			else
 			    echo "Exit"
 			fi
-            break
-            ;;
-        "Update")
+        elif [[ "$CHOICE" == *"Update"* ]]; then
             sudo apt update
             sudo snap refresh 
-            break
-            ;;
-        "Visual Studio Code Extensions")
-            #vscode extensions
-            sudo code --install-extensions ms-vscode.cpptools formulahendry.code-runner esbenp.prettier-vscode donjayamanne.git-extension-pack ms-vscode.cmake-tools
-            break
-            ;;
-        "Exit")
-            break
-            ;;
-        *) echo "Invalid Operation $REPLY";;
-    esac
-done
-
+        elif [[ "$CHOICE" == *"Visual Studio Code Extensions"* ]]; then
+            sudo code --install-extensions ms-vscode.cpptools formulahendry.code-runner esbenp.prettier-vscode donjayamanne.git-extension-pack ms-vscode.cmake-tools twxs.cmake
+        fi
+    fi
+else
+  echo "Exit"
+fi
